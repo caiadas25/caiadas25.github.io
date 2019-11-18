df12774a9c4039d5c55b57f08ef40e6de940e5db
Nuno Caiadas <nuno.caiadas@mercedes-benz.io>
2019-11-18 15:04:40 +0000

    Added JSON to HTML functionality for projects section

---
+++ main.js
@@ -0,0 +1,33 @@
+fetch('projects.json')
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  getProjects(data);
+})
+.catch(function (err) {
+  console.log(err);
+});
+
+
+async function getProjects(){
+const response = await fetch('projects.json');
+const data = await response.json();
+
+const mainContainer = document.getElementById("project-container");
+
+  for (let i = 0; i < data.length; i++) {
+  // create div
+  const div = document.createElement('div');
+  div.setAttribute('class', 'project');
+  div.innerHTML =   '<div class="project-text">'+
+                        '<span class="project-title">' + data[i].title + '</span>' +
+                        '<p class="project-description">' + data[i].description + '</p>' + 
+                        '<a href="' + data[i].links + '" class="project-links">' + data[i].linkText + '</a>' +
+                    '</div>'+
+                    '<img class="project-image" src="' + data[i].imagePath + '"/>';
+  // add div to main container
+  mainContainer.appendChild(div);
+}
+}
+
